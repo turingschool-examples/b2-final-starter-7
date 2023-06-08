82b9be386990fc4380e94be1c95f715b18cb6d10
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-08 14:46:31 -0600

    add seed #destroy_all to all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Transaction.destroy_all
+InvoiceItem.destroy_all
+Item.destroy_all
+Invoice.destroy_all
+Merchant.destroy_all
+Customer.destroy_all
 
 Rake::Task["csv_load:all"].invoke
