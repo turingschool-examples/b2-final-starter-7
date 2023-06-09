1a892bf86c6a7aa905cfe76d16112a1d9108cb80
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-09 16:44:22 -0600

    test relationships & validation

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Coupon, type: :model do
+  before(:each) do 
+    @merchant = create(:merchant)
+    @coupon_1 = @merchant.coupons.create!(name: '20%', unique_code: '20OFF', percent_discount: 20, dollar_discount: nil)
+    @coupon_2 = @merchant.coupons.create!(name: '50%', unique_code: '50OFF', percent_discount: 50, dollar_discount: nil)
+  end
+  describe 'relationships' do 
+    it {should belong_to :merchant}
+    it {should belong_to(:invoice).optional }
+  end
+
+  describe 'validations' do 
+    it {should validate_uniqueness_of :unique_code} 
+  end
+end
