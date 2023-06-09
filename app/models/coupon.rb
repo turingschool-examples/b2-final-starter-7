89a0d03b1fd3810caf9c77a7bd99bd184adf757b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-09 17:19:45 -0600

    test and add status enum/

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,5 +2,5 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   belongs_to :invoice, optional: true
   validates :unique_code, uniqueness: true
-  enum status: {enabled: 1, disabled: 0}
+  enum status: [:disabled, :enabled]
 end
