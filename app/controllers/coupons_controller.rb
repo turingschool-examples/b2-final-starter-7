26ebce20fe59f8cadca94de0ef3aeada048abee9
Amy Spears <amspears007@gmail.com>
2023-06-09 15:47:28 -0600

    Iterate through coupons to render attributes

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,6 @@
 class CouponsController < ApplicationController
   def index
-
+    @merchant = Merchant.find( params[:merchant_id])
+    @coupons = @merchant.coupons
   end
 end
---
+++ spec/features/coupons/index_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "Coupon Index" do
+  before :each do
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    @coupon1 = @merchant1.coupons.create!(name: "Ten Percent Off", unique_code: "10%OFF", discount: 1, status: 1)
+    @coupon2 = @merchant1.coupons.create!(name: "Five Percent Off", unique_code: "5%OFF", discount: 1, status: 1)
+    @coupon3 = @merchant1.coupons.create!(name: "Fifteen Percent Off", unique_code: "15%OFF", discount: 1, status: 1)
+    @coupon4 = @merchant1.coupons.create!(name: "Ten Dollars Off", unique_code: "10$OFF", discount: 0, status: 1)
+    @coupon5 = @merchant1.coupons.create!(name: "Twelve Percent Off", unique_code: "12%OFF", discount: 1, status: 0)
+
+  end
+
+  describe "US1 merchant_coupons_path(@merchant1)" do
+    it "I see all of my coupon names including their amount off And each coupon's name is also a link to its show page." do
+      visit merchant_coupons_path(@merchant1)
+
+      expect(page).to have_content("Coupon Index Page")
+      
+      expect(page).to have_content("Ten Percent Off")
+    end
+  end
+end
