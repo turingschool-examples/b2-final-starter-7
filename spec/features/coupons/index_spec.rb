67ca12b1996a7333f7964cbfef5aa73e19f8861e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-09 18:26:59 -0600

    update test to agree with database

--- spec/features/coupons/index_spec.rb
+++ spec/features/coupons/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Coupons Index', type: :feature do
   before :each do 
     @dolly = create(:merchant)
-    @coupon_1 = @dolly.coupons.create!(name: 'Labor Day', unique_code: '20OFF', percent_discount: 20, dollar_discount: nil)
-    @coupon_2 = @dolly.coupons.create!(name: 'Memorial Day', unique_code: '50OFF', percent_discount: 50, dollar_discount: nil)
+    @coupon_1 = @dolly.coupons.create!(name: 'Labor Day', unique_code: '20OFF', discount: 20, discount_type: 0)
+    @coupon_2 = @dolly.coupons.create!(name: 'Memorial Day', unique_code: '50OFF', discount: 50, discount_type: 1)
   end
 
   describe 'merchant coupons' do 
@@ -13,9 +13,9 @@ RSpec.describe 'Coupons Index', type: :feature do
 
       within("#coupons") do 
         expect(page).to have_content(@coupon_1.name)
-        expect(page).to have_content(@coupon_1.percent_discount)
+        expect(page).to have_content(@coupon_1.discount)
         expect(page).to have_content(@coupon_2.name)
-        expect(page).to have_content(@coupon_1.percent_discount)
+        expect(page).to have_content(@coupon_1.discount)
       end
     end
   end
