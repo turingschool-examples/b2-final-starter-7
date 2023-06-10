c8aef659163976cc07ff80945aaa850ff1b72e33
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-09 18:21:21 -0600

    define enum

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,4 +3,5 @@ class Coupon < ApplicationRecord
   belongs_to :invoice, optional: true
   validates :unique_code, uniqueness: true
   enum status: [:disabled, :enabled]
+  enum discount_type: ["Percentage", "Dollar Amount"]
 end
