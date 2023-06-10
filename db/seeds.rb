6b48ebed0d2abd30b5cb3269444e5325272f9829
Amy Spears <amspears007@gmail.com>
2023-06-09 18:13:41 -0600

    Add coupon show page header and seed coupon data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,11 @@ Item.destroy_all
 Coupon.destroy_all
 Merchant.destroy_all
 Customer.destroy_all
+
+@merchant1 = Merchant.create!(name: "Dolly Parton")
+@coupon1 = @merchant1.coupons.create!(name: "Ten Percent Off", unique_code: "10%OFF", amount_off: 10, discount: 1, status: 1)
+@coupon2 = @merchant1.coupons.create!(name: "Five Percent Off", unique_code: "5%OFF", amount_off: 5, discount: 1, status: 1)
+@coupon3 = @merchant1.coupons.create!(name: "Fifteen Percent Off", unique_code: "15%OFF", amount_off: 15, discount: 1, status: 1)
+@coupon4 = @merchant1.coupons.create!(name: "Ten Dollars Off", unique_code: "10$OFF", amount_off: 10, discount: 0, status: 1)
+@coupon5 = @merchant1.coupons.create!(name: "Twelve Percent Off", unique_code: "12%OFF", amount_off: 12, discount: 1, status: 0)
 Rake::Task["csv_load:all"].invoke
