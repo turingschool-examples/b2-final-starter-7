f52231485ef86ba04fe6c20f71dbe2924b79326a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-10 14:31:55 -0600

    Test: coupon name is link to coupon show page

--- spec/features/coupons/index_spec.rb
+++ spec/features/coupons/index_spec.rb
@@ -10,12 +10,24 @@ RSpec.describe 'Coupons Index', type: :feature do
   describe 'merchant coupons' do 
     it 'lists coupon names and amount off' do 
       visit(merchant_coupons_path(@dolly))
-
+      
       within("#coupons") do 
         expect(page).to have_content(@coupon_1.name)
         expect(page).to have_content(@coupon_1.discount)
         expect(page).to have_content(@coupon_2.name)
         expect(page).to have_content(@coupon_1.discount)
+      end
+    end
+    
+    it 'coupon names are links to coupon show pages' do 
+      visit(merchant_coupons_path(@dolly))
+
+      within("#coupon-#{@coupon_1.id}") do 
+        expect(page).to have_link("#{@coupon_1.name}", href: "merchants/#{@dolly.id}/coupons/#{@coupon_1.id}")
+      end
+
+      within("#coupon-#{@coupon_2.id}") do 
+        expect(page).to have_link("#{@coupon_2.name}", href: "merchants/#{@dolly.id}/coupons/#{@coupon_2.id}")
       end
     end
   end
