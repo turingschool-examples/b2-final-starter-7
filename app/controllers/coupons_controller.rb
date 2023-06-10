4b55ccf6f3a2e06ecbd5f6558df0e8be6c3e9db3
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-10 14:42:25 -0600

    finish us 1

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,4 +2,8 @@ class CouponsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
 end
