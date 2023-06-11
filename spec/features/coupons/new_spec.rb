6ab218119c1acc5e59b51723ea06d1fa50aa49ba
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-10 18:43:39 -0600

    change flash message test

--- spec/features/coupons/new_spec.rb
+++ spec/features/coupons/new_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe 'New Coupon Form', type: :feature do
   before :each do 
     @dolly = create(:merchant)
+    @jermaine = create(:merchant)
     @coupon_1 = create(:coupon, merchant: @dolly)
     @coupon_2 = create(:coupon, merchant: @dolly)
   end
 
   describe 'Form works' do 
     it 'form can be filled in' do 
-      visit (new_merchant_coupon_path(@dolly))
+      visit new_merchant_coupon_path(@dolly)
 
       expect(page).to have_content("Create New Coupon")
       expect(page).to have_content('Name')
@@ -27,6 +28,26 @@ RSpec.describe 'New Coupon Form', type: :feature do
 
       expect(current_path).to eq(merchant_coupons_path(@dolly))
       expect(@dolly.coupons.count).to eq(3)
+    end
+
+    it 'error if more than 5 active coupons created' do 
+      coupon1 = create(:coupon, status: 1, merchant: @jermaine)
+      coupon2 = create(:coupon, status: 1, merchant: @jermaine)
+      coupon3 = create(:coupon, status: 1, merchant: @jermaine)
+      coupon4 = create(:coupon, status: 1, merchant: @jermaine)
+      coupon5 = create(:coupon, status: 1, merchant: @jermaine)
+
+      visit new_merchant_coupon_path(@jermaine)
+
+      fill_in(:name, with: 'Ides of March')
+      fill_in(:unique_code, with: 'IDES15')
+      fill_in(:discount, with: 15)
+      select 'Percentage', from: :discount_type
+      select 'Active', from: :status
+      
+      click_button('Save')
+      expect(current_path).to eq(new_merchant_coupon_path(@jermaine))
+      expect(page).to have_content("Coupon could not be saved because you have too many coupons active currently")
     end
   end
   
