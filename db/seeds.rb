83a7625ac80292d85ee7b645b07f5481e3226545
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-11 21:51:19 -0600

    attempt seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Rake::Task["csv_load:all"].invoke
+
+  @merchant = Merchant.create!(name: 'Jeremicah')
+  @customer = Customer.create!(first_name: 'Alice', last_name: 'Walker', address: 'a string', city: 'a city', state: 'a state', zip: 78901 )
+  @coupon = Coupon.create!(name: 'Labor Day', unique_code: '20OFF', discount: 20, discount_type: 0, merchant: @merchant)
+  @invoice_1 = Invoice.create!(customer_id: @customer.id, status: 2, coupon: @coupon)
+  @invoice_2 = Invoice.create!(customer_id: @customer.id, status: 2, coupon: @coupon)
+  @invoice_3 = Invoice.create!(customer_id: @customer.id, status: 2, coupon: @coupon)
+  @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
+  @transaction2 = Transaction.create!(credit_card_number: 230948, result: 1, invoice_id: @invoice_2.id)
+  @transaction3 = Transaction.create!(credit_card_number: 234092, result: 0, invoice_id: @invoice_3.id)
