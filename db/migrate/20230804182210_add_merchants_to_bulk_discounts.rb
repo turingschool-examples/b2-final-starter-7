3fadd215fe691fdba2110b8d340067c33a518604
Ian Lyell <ian.lyell@yahoo.com>
2023-08-04 13:32:08 -0500

    Finish model and test setup for db

--- db/migrate/20230804182210_add_merchants_to_bulk_discounts.rb
+++
@@ -1,5 +0,0 @@
-class AddMerchantsToBulkDiscounts < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bulk_discounts, :merchants, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_182210) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_183033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_182210) do
     t.float "percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "merchants_id", null: false
-    t.index ["merchants_id"], name: "index_bulk_discounts_on_merchants_id"
+    t.bigint "merchant_id", null: false
+    t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -83,7 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_182210) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
-  add_foreign_key "bulk_discounts", "merchants", column: "merchants_id"
+  add_foreign_key "bulk_discounts", "merchants"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "items", "merchants"
