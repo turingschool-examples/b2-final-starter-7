b0f06abd9000c30a21fe0301b1d89012e9b6f744
Gabriel Torres <thomastgtorres21@gmail.com>
2023-08-04 11:24:19 -0500

    start welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get "/", to: "welcome#index"
+  get "/merchants/:merchant_id/dashboard", to: "dashboard#index"
+  get "/admin/merchants", to: "admin/merchants#index"
+  get "/admin/dashboard", to: "admin/dashboard#index"
+
   resources :merchants, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
