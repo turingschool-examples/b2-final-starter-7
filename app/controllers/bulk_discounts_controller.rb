ef740043b4c7867d3af71267bf2a4cb826435d39
Anna Wiley <awiley33@gmail.com>
2023-08-05 11:51:09 -0500

    feat: Add functionality for user story 2

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -8,4 +8,31 @@ class  BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.new(new_bulk_discount_params)
+    if @bulk_discount.save
+      flash.notice = "New Discount Successfully Created!"
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else
+      flash.notice = "Please complete all fields to continue."
+      render :new
+    end
+
+  end
+
+  private
+
+  def new_bulk_discount_params
+    params.permit(:percentage_discount, :quantity_threshold)
+  end
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percentage_discount, :quantity_threshold)
+  end
 end
