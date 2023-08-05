49924f70378266ec4f37cafe3202833e45765043
Matthew Lim <matlim200@gmail.com>
2023-08-04 20:33:34 -0700

    Test: Add initial test setup for user story 5

--- spec/features/bulk_discounts/bulk_discounts_spec.rb
+++ spec/features/bulk_discounts/bulk_discounts_spec.rb
@@ -112,4 +112,26 @@ describe "Bulk Discounts" do
       end
     end
   end
+
+  #5: Merchant Bulk Discount Edit
+
+  describe "As a merchant" do
+    describe "When I visit my bulk discount show page" do
+      describe "Then I see a link to edit the bulk discount" do
+        describe "When I click this link" do
+          describe "Then I am taken to a new page with a form to edit the discount" do
+            describe "And I see that the discounts current attributes are pre-poluated in the form" do
+              describe "When I change any/all of the information and click submit" do
+                describe "Then I am redirected to the bulk discount's show page" do
+                  it "And I see that the discount's attributes have been updated" do
+
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
