e5be5e6fb9483aad5f2c8a16dfdbfe02271ac25f
Kaina Cockett <kainacockett@gmail.com>
2023-08-05 16:28:00 -0600

    refac: more robust testing of models

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,5 +2,5 @@ class Discount < ApplicationRecord
   belongs_to :merchant
 
   validates :percentage, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :threshold, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :threshold, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -5,7 +5,7 @@ describe Discount do
     it { should validate_presence_of(:percentage) }
     it { should validate_numericality_of(:percentage).only_integer.is_greater_than(0) }
     it { should validate_presence_of(:threshold) }
-    it { should validate_numericality_of(:threshold).only_integer.is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:threshold).only_integer.is_greater_than(0) }
   end
 
   describe "relationships" do
