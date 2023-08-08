020ed5de9349bb643128aa0c74082b9a21b859a5
Connor Richmond <richmond.connor@gmail.com>
2023-08-08 18:26:15 -0400

    test show page

---
+++ spec/features/discounts/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Discount Show Page', type: :feature do
+  before :each do
+    @merchant1 = Merchant.create!(name: "Hair Care")
+
+    @discount1 = @merchant1.discounts.create!(percent_discount: 10, threshold_quantity: 10)
+
+    visit merchant_discount_path(@merchant1, @discount1)
+  end
+
+  it "shows discount quantity threshold and percentage discount" do
+    expect(page).to have_content("Discount: #{@discount1.percent_discount}%", count: 1)
+    expect(page).to have_content("Quantity Threshold: #{@discount1.threshold_quantity}", count: 1)
+  end
+end
