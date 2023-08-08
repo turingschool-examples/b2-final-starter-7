2a9bea5cbcea3c3e3b640ac6c8430f6a5be7b44d
Connor Richmond <richmond.connor@gmail.com>
2023-08-08 17:39:45 -0400

    testing and model tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 describe Merchant do
+
   describe "validations" do
     it { should validate_presence_of :name }
   end
-  describe "relationships" do
+
+describe "relationships" do
     it { should have_many :items }
     it { should have_many(:invoice_items).through(:items) }
-    it {should have_many(:invoices).through(:invoice_items)}
+    it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
-
+    it { should have_many(:discounts) }
   end
 
   describe "class methods" do
