2ded9dd06c509b332dab6ff39719c871ae085c25
Connor Richmond <richmond.connor@gmail.com>
2023-08-08 21:15:08 -0400

    seeded discounts and fix method to only apply highest discount

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+
 Rake::Task["csv_load:all"].invoke
+
+@merchant = Merchant.first
+
+@discount1 = @merchant.discounts.create!(percent_discount: 10, threshold_quantity: 5)
+@discount2 = @merchant.discounts.create!(percent_discount: 15, threshold_quantity: 9)
+@discount3 = @merchant.discounts.create!(percent_discount: 40, threshold_quantity: 10)
+@discount4 = @merchant.discounts.create!(percent_discount: 70, threshold_quantity: 15)
