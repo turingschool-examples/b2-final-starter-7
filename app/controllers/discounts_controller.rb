05bcf64a6193f3a73ded97708a245099973b22b6
pcbennett108 <pcbennett42@gmail.com>
2023-09-26 22:13:01 -0600

    feat: add link on show page to discount update form, add update to discounts controler

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -21,6 +21,19 @@ class DiscountsController < ApplicationController
       threshold: params[:threshold],
       merchant: @merchant)
       redirect_to merchant_discounts_path(@merchant)
+    end
+    
+  def edit
+    @discount = Discount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+  
+  def update
+    @discount = Discount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount.update(discount_params)
+    flash.notice = 'Discount Has Been Updated!'
+    redirect_to merchant_discount_path(@merchant, @discount)
   end
 
   def destroy
