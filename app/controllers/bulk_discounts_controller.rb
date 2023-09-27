ce4b8274e1b4a028c7a736af224cc78d0aef4deb
Cory Powell <coryrpowell@gmail.com>
2023-09-26 22:06:33 -0400

    added the new page for bulk discounts view and the controller for the new and create methods as well as a link to a form in the index that takes you to the new.html.erb

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,10 +1,20 @@
 class BulkDiscountsController < ApplicationController
   def index
     @bulk_discounts = BulkDiscount.all
+    @merchant = Merchant.find(params[:id])
   end
 
   def show
     @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def new
+   @merchant = Merchant.find(params[:id])
+   
+  end
+
+  def create
+
   end
 
 end
